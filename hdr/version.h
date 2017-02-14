4f50832ab53cb2e4bcf48c5277216478a078740f
KJD <PerditionC@gmail.com>
2017-02-14 00:34:57 -0500

    kernel branding, FreeDOS vs DOSC

--- hdr/version.h
+++ hdr/version.h
@@ -27,6 +27,9 @@
 /****************************************************************/
 
 /* The version the kernel reports as compatible with */
+/* The version of DOS kernel reported as compatible with to */
+/* applications - major.minor.revision                      */
+/* DOS version 7.10 with FAT32 support or 6.22 otherwise    */
 #ifdef WITHFAT32
 #define MAJOR_RELEASE   7
 #define MINOR_RELEASE   10
@@ -35,17 +38,30 @@
 #define MINOR_RELEASE   22
 #endif
 
-/* The actual kernel revision, 2000+REVISION_SEQ = 2.REVISION_SEQ */
+/* The actual kernel revision */
+#ifdef OEM_DOSC
+/* 3000+REVISION_SEQ = 3.REVISION_SEQ */
+#define REVISION_SEQ    0       /* returned in BL by int 21 function 30 */
+#define OEM_ID          0xdc    /* FreeDOS, returned in BH by int 21 30 */
+#define OEM_NAME "DOS-C"
+#define OEM_BUILD "3."
+#else
+/* 2000+REVISION_SEQ = 2.REVISION_SEQ */
 #define REVISION_SEQ    42      /* returned in BL by int 21 function 30 */
 #define OEM_ID          0xfd    /* FreeDOS, returned in BH by int 21 30 */
+#define OEM_NAME "FreeDOS"
+#define OEM_BUILD "20"
+#endif
+
 
 /* Used for version information displayed to user at boot (& stored in os_release string) */
 #ifndef KERNEL_VERSION
-#define KERNEL_VERSION "- SVN "
+#define KERNEL_VERSION "- VCS " /* Version Control System, e.g. SVN,GIT,... */
 #endif
 
-/* actual version string */
-#define KVS(v,s,o) "FreeDOS kernel " v "(build 20" #s " OEM:" #o ") [compiled " __DATE__ "]\n"
-#define xKVS(v,s,o) KVS(v,s,o)
-#define KERNEL_VERSION_STRING xKVS(KERNEL_VERSION, REVISION_SEQ, OEM_ID)
+
+/* actual version string, displayed at signon & via FD int 21h/ax=33FFh */
+#define KVS(v,s,o,n,b) n " kernel " v "(build " b #s " OEM:" #o ") [compiled " __DATE__ "]\n"
+#define xKVS(v,s,o,n,b) KVS(v,s,o,n,b)
+#define KERNEL_VERSION_STRING xKVS(KERNEL_VERSION, REVISION_SEQ, OEM_ID, OEM_NAME, OEM_BUILD)
 
